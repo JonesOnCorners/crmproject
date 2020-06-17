dcc08bcd7064276b512585f163a21d28a2cd5625
unknown <USER@SYNPUNLT1911068.int.asurion.com>
2020-06-17 16:39:08 +0530

    changed the STATICFILE_STORAGE as per whitenoise docs

--- crmproject/settings.py
+++ crmproject/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/images/'
 
